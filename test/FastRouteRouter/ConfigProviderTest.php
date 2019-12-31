69a1f898f343a8d84142dd3810d58b54e9a69729
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:43:53 -0600

    Rewriting as Laminas Project package

--- test/FastRouteRouter/ConfigProviderTest.php
+++ test/FastRouteRouter/ConfigProviderTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-fastroute for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-fastroute/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-fastroute for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-fastroute/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-fastroute/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Router\FastRouteRouter;
+namespace MezzioTest\Router\FastRouteRouter;
 
+use Mezzio\Router\FastRouteRouter;
+use Mezzio\Router\FastRouteRouter\ConfigProvider;
+use Mezzio\Router\RouterInterface;
 use PHPUnit\Framework\TestCase;
-use Zend\Expressive\Router\FastRouteRouter;
-use Zend\Expressive\Router\FastRouteRouter\ConfigProvider;
-use Zend\Expressive\Router\RouterInterface;
 
 class ConfigProviderTest extends TestCase
 {
