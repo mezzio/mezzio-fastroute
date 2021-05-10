b8d119ccc15a46c612d417f6b22db93e00ca3f2d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:02:05 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/FastRouteRouterFactory.php
+++ src/FastRouteRouterFactory.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-fastroute for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-fastroute/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-fastroute/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Mezzio\Router;
